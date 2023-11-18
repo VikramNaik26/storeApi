fd1265f83d64e470141ff82835252a4a00b16979
Vikram Naik <26vikramnaik@gmail.com>
2023-11-18 23:31:56 +0530

    Basic set up

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log('04 Store API')
+require("dotenv").config();
+// aysnc errors
+
+const express = require("express");
+const app = express();
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send('<h1>Store Api</h1><a href="/api/v1/products">Store</a>');
+});
+
+// products routes
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = () => {
+  try {
+    // connectDb
+    app.listen(port, () => {
+      console.log("server listening on port 3000");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
